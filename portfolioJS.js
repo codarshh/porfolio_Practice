aa49bd59ad5e2f3edc64cd0982ca9d8fdafdc846
Adarsh Kumar <aadarshkumar2131@gmail.com>
2026-05-24 09:56:42 +0530

    Implemented header section

--- portfolioJS.js
+++ portfolioJS.js
@@ -1,15 +1,59 @@
-const preloader  = document.querySelector('[data-preloader]');
+// const preloader  = document.querySelector('[data-preloader]');
 
-// window.addEventListener("DOMContentLoaded", function (){
-//   preloader.classList.add("loaded");
-//   document.body.classList.add("loaded"); 
+// // window.addEventListener("DOMContentLoaded", function (){
+// //   preloader.classList.add("loaded");
+// //   document.body.classList.add("loaded"); 
+// // });
+
+// window.addEventListener("load", function () {
+
+//     setTimeout(() => {
+//         preloader.classList.add("loaded");
+//         document.body.classList.add("loaded");
+//     }, 1000);
+
 // });
 
-window.addEventListener("load", function () {
+
+
+// *************     START AGAIN FROM HERE    ***********
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("DOMContentLoaded", function () {
 
     setTimeout(() => {
         preloader.classList.add("loaded");
         document.body.classList.add("loaded");
-    }, 1500);
+    }, 1000);
 
+});
+
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+addEventOnElements(navTogglers, "click", function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+});
+
+addEventOnElements(navLinks, "click", function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.classList.remove("nav-active");
+});
+
+const header = document.querySelector("[data-header]");
+window.addEventListener("scroll", function () {
+  header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
 });
